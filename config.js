b254b7b58486e000eb2cf63c80b975d81f99d9c7
Peter Chien <pchien@pocketgems.com>
2013-07-06 20:15:49 -0700

    convert tech to modifiers, start some framework for events

---
+++ config.js
@@ -0,0 +1,22 @@
+var villainTechTree = [
+    {'id': 'reload', 'description': 'Traps reload faster', 'cost': 1, 'children': ['research']},
+    {'id': 'research', 'description': 'More tech per level', 'cost': 1, 'children': []},
+    {'id': 'cheap', 'description': 'Traps are cheaper', 'cost': 1, 'children': ['score']},
+    {'id': 'score', 'description': 'Bonus points per level', 'cost': 1, 'children': []} // no effect yet
+];
+
+var events = {
+    'henchpeople': {
+	'text': 'Some of your female henchmen are displeased with the terminology and are demanding you refer to them as henchpeople.',
+	'options': [
+	    {'text': 'Fine. All genders are equal to me - AS MY SLAVES!', 'effect': 'henchpeople'},
+	    {'text': 'I do not have time for this.', 'effect': 'tempReduceCurrency|minions:2'}
+	]
+    }
+};
+
+var temporaryModifiers = [
+    'tempReduceCurrency'
+];
+
+// implemented modifiers: reload, research, cheap, tempReduceCurrency
